6e02e7d045ff6435aed04e30e775f3af9a4e117a
Ram Krishnan <ramkrishnan@live.com>
2020-08-07 23:41:27 +0530

    Refactor form requests to have consistent max string value - 256

--- app/Http/Requests/Restaurant/RegisterFormRequest.php
+++ app/Http/Requests/Restaurant/RegisterFormRequest.php
@@ -10,10 +10,10 @@ class RegisterFormRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:restaurants|unique:customers',
-            'phone' => 'required|string|max:255',
-            'address' => 'required|string|max:255',
+            'name' => 'required|string|max:256',
+            'email' => 'required|string|email|max:256|unique:restaurants|unique:customers',
+            'phone' => 'required|string|max:256',
+            'address' => 'required|string|max:256',
             'cuisine' => ['required', Rule::in(['vegetarian', 'non-vegetarian'])],
             'password' => 'required|string|min:8|confirmed',
             'image' => 'nullable|image',
