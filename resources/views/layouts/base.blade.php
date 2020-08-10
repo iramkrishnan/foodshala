4a34c8315d025ee55dcb61bc3ab4bcb98bcfbd65
Ram Krishnan <ramkrishnan@live.com>
2020-08-10 22:02:41 +0530

    Add disclaimer

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -25,6 +25,15 @@
 </style>
 <body class="flex flex-col min-h-screen">
 
+
+<div class=" text-center pt-2 lg:px-4">
+    <div class="p-2 bg-blue-500 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex" role="alert">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-1.31 7.526c-.099-.807.528-1.526 1.348-1.526.771 0 1.377.676 1.28 1.451l-.757 6.053c-.035.283-.276.496-.561.496s-.526-.213-.562-.496l-.748-5.978zm1.31 10.724c-.69 0-1.25-.56-1.25-1.25s.56-1.25 1.25-1.25 1.25.56 1.25 1.25-.56 1.25-1.25 1.25z"/></svg>
+        <span class=" font-semibold mr-2 text-left flex-auto mx-2">This project is for demonstration purposes only. You can order your favourite food, and then go to the kitchen and cook it for yourself! &#128578; </span>
+    </div>
+</div>
+
+
 <!-- Navbar for Restaurants -->
 
 @if(\Illuminate\Support\Facades\Auth::guard('restaurant')->check())
